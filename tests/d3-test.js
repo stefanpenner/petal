e8bdb5686fa8e07e9c14c890e8f0c38e8305ff01
Adolfo Builes <builes.adolfo@gmail.com>
2014-06-13 17:32:21 -0300

    Add failing test with d3.

---
+++ tests/d3-test.js
@@ -0,0 +1,13 @@
+var Leaf = require('../index.js');
+var assert = require('assert');
+var fs     = require('fs');
+
+describe('Leaf - d3', function() {
+  it("includes an anonymous module", function() {
+    var source = fs.readFileSync("bower_components/d3/d3.js");
+    var m = new Leaf("bower_components/d3/d3.js", source);
+
+    assert(m.hasDefine, true, 'module has define property');
+    assert.equal(m.isAnonymous, true, 'module is not named');
+  });
+});
